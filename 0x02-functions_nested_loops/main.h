de7316145d5198afbdb4e99ea0b552e9ad07d8ce
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 <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>
2022-12-13 21:42:18 +0300

    Task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,15 +3,17 @@
 
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _islower(c);
-int _isalpha(c);
+int _islower(int c);
+int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void pritn_to_98(int n);
-int _putchar(char c);
+void print_to_98(int n);
+int _putchar(char);
+void print_times_table(int n);
+void num(int r, int n);
 
 #endif

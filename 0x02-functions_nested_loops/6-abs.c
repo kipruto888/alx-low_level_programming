5600ee1a4e9ef718b5eeb0f331541de28187728e
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 <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>
2022-12-14 10:03:32 +0300

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @r: The integer to be computed
+ * Retrun: The absolute value
+*/
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (-r);
+}

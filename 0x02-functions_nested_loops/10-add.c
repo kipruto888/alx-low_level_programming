90f14c3c064b4e662907531082f135a56a7aeb5f
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 <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>
2022-12-14 11:04:14 +0300

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - add two numbers
+ * @r: the first numer
+ * @k: the second number
+ * Return: the addition of both
+*/
+int add(int r, int k)
+{
+	return (r + k);
+}

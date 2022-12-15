32aa7eeefc18061ba2e4eee1eb0c1c5aeb12497c
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 <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>
2022-12-15 19:57:07 +0300

    Task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* print_diagonal - prints diagonal line and times.
+* @n: times diagonal line is printed.
+* Return: no return.
+*/
+void print_diagonal(int n)
+{
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar(92);
+		if (i < (n - 1))
+			_putchar('\n');
+	}
+	_putchar('\n');
+}

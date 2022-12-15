afc3af3c2d80a269a97ada905baed3e0c197e66b
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 <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>
2022-12-15 19:49:06 +0300

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_line - prints straight line and times
+* @n: times straight line in printed.
+* Return: no return.
+*/
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}

2b676a31edebe786cfe36695f0a1b5652ef0c002
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 <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>
2022-12-22 14:05:23 +0300

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,20 +1,21 @@
 #include "main.h"
 /**
- * _strcat - concatenates the string pointed to by @src, including the terminat
- * null byte, to the end of the string pointed to by @dest.
+ * _strcat - concatenates two strings.
  * @dest: A pointer to the string to be concatenated upon.
  * @src: The source string to be appended to @dest.
- * Return: A pointer to the destination string @dest.
+ * Return: void.
  */
-char *_strcat(char *dest, const char *src)
+char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
+	int a = -1, i;
 
-	while (dest[index++])
-		dest_len++;
+	for (i = 0; dest[i] != '\0';)
+		i++;
+	do {
+		a++;
+		dest[i] = src[a];
+		i++;
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-
+	} while (src[a] != '\0');
 	return (dest);
 }

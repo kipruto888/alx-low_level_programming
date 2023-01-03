f8ee3a75a233c081742dcda7545a210c0db8d5a6
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 <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>
2023-01-03 15:38:09 +0300

    Task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -3,8 +3,9 @@
 * _strstr - first occurrence of the substring needle in the string haystack.
 * @haystack: main str to be examined.
 * @needle: searched in haystack.
-* Return: 0
-*/
+* Return: return 0
+**/
+
 char *_strstr(char *haystack, char *needle)
 {
 	char *str1, *str2;

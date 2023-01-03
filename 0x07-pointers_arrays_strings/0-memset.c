d0020d1041369b6cee197e5b019d91b882cca9c5
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 <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>
2023-01-03 14:30:15 +0300

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,13 +3,13 @@
 * _memset - Fills the first n bytes of the memory area
 * pointed to by @s with the constant byte @c.
 *@s: A pointer to the memory area to be filled.
-*@c: The character to fill the memory area with.
+*@b: The character to fill the memory area with.
 *@n: The number of bytes to be filled.
 * Return: A pointer to the filled memory area @s.
 */
-void *_memset(void *s, int c, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int a;
+	unsigned int a = 0;
 
 	for (a = 0; a < n; a++)
 	{

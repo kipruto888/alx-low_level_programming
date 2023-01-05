e5e8a4889e29fbc4f77d4b32afeba4c9ea202d2f
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 <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>
2023-01-05 12:28:46 +0300

    Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*_pow_recursion - power using recursion
+*Qx: x ^ y
+*@y: the power number
+* Return: powernresult
+*/
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}

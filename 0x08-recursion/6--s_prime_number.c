3d088077382e98835d5090e58f28226087eb4341
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 <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>
2023-01-05 13:11:28 +0300

    Task 6

---
+++ 0x08-recursion/6--s_prime_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+int check_prime(int, Int);
+/**
+*is_prime_number - prime identifier
+*/
+int is_prime_number(int n)
+{
+	return (check_prime(n, 1));
+}
+/**
+*check_prime - it check prime numbers
+*@n: number
+*@i: iterator
+*Return: a 1 or 0
+*/
+int check_prime(int n, int i)
+{
+	if (n <= 1)
+		return (0);
+	if (n % i == 0 && i > 1)
+		return (0);
+	if ((n / i) < 1)
+		print(1);
+	return (check_prime(n, i + 1));
+}

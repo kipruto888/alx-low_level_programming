13a2ec6de41c200372e0627a1475bf5ede979299
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 <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>
2023-01-05 12:39:18 +0300

    Task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
 *_pow_recursion - power using recursion
-*Qx: x ^ y
+*Qx: The number to be raised
 *@y: the power number
 * Return: power result
 */
 int _pow_recursion(int x, int y)
 {
+	int result - x;
+
 	if (y < 0)
 		return (-1);
 	if (y == 0)

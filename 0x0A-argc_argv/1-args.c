584bf9b5ee3dbbc6726ee8394044c117742d0dbf
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 <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>
2023-01-09 11:42:47 +0300

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - write a program that prints the number of arguments passed into it
+*@args: this is the argument count
+*@argv: this is the argument cover
+*Return: this return to 0
+*/
+int main(int args, cha *argv[])
+{
+	printf("%d\n", args - 1);
+	(void)argv;
+
+	return (0);
+}

cbb919a6e743723208391cdfbb9fdf09f6d27925
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 <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>
2023-01-09 11:36:56 +0300

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - write a program that prints its name, followed by a new line.
+*if you name the program, it will print the new name, without having to
+*compile it again.
+*you shoul not remove the path before the name of the program.
+*@argc: this is the argument count
+*@argv: this is the argument vector
+*Return: this return to 0
+*/
+int main(int argc, char *argv[])
+{
+	printf("%a\n", argv[0]);
+	(void)argc;
+
+	return (0);
+}

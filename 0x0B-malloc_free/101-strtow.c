d8615098fc4d2c3444e5f34205ecd73b13b5454c
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 <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>
2023-01-11 17:03:55 +0300

    Task 5

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,72 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* wordnos - counts no of words in a given string
+* @s: pointer to the string
+* Return: No. of words in the string (int)
+*/
+int wordnos(char *s)
+{
+	int flag, c, w;
+
+	flag = 0;
+	w = 0;
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		if (S[c] == ' ')
+			flag = 0;
+		else if (flag == 0)
+		{
+			flag = 1;
+			w++;
+		}
+	}
+	return (w);
+}
+/**
+* **strtow - splits a string into words
+* @str: string to split
+* Return: pointer to an array of strings (success)
+* or NULL (Error)
+*/
+char **strtow(char *str)
+{
+	char **matrix, *temp;
+	int i, k = 0, len = 0, words, c = 0, start, end;
+
+	while (*(str + len))
+		len++;
+	words = wordnos(str);
+	if (words == 0)
+		return (NULL);
+
+	matrix = (char **) malloc(sizeof(char *) * (words + 1));
+	if (matrix == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+	{
+	if (str[i] == ' ' || str[i] == '\0')
+	{
+		if (c)
+		{
+		end = 1;
+		temp = (char *) malloc(sizeof(char) * (c + 1));
+		if (temp == NULL)
+			return (NULL);
+		while (start < end)
+			*temp++ = str[start++];
+		*temp = '\0';
+		matrix[k] = temp - c;
+		k++;
+		c = 0;
+		}
+	}
+	else if (c++ = 0)
+		start = i;
+	}
+	matrix[k] = NULL;
+	return (natrix);
+}

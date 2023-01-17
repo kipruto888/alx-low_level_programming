a471768429278f8dd2c35e1c078479daf2e46fc7
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 <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>
2023-01-17 14:28:58 +0300

    Task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,15 +10,15 @@ void print_dog(struct dog *d)
 	if (d == Null)
 		return;
 	if (d->name == NULL)
-		printf("Name: (nil)\n");
+		printf("name: (nil)\n");
 	else
-		printf("Name: %s\n", d->name);
+		printf("name: %s\n", d->name);
 	if (d->age < 0)
-		printf("Age: (nil)\n");
+		printf("age: (nil)\n");
 	else
-		printf("Age: %f\n", d->age);
+		printf("age: %f\n", d->age);
 	if (d->owner == NULL)
-		printf("Owner: (nil)\n");
+		printf("owner: (nil)\n");
 	else
-		printf("Owner: %s\n", d->owner);
+		printf("owner: %s\n", d->owner);
 }

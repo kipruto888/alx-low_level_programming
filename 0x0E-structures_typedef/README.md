f02264643ec6b309e87a46944587cd43d39d62c1
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 <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>
2023-01-17 13:55:25 +0300

    Task 0

--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -1,1 +1,1 @@
-0x0E-Structures, Typedef
+0x0E-Structures, typedef
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,19 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
 * struct dog - Struct dog
 * @name: name
 * @age: age
 * @owner: owner
-* Description: This struct is for dog
+* Description: This struct is dog
 */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
+
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
